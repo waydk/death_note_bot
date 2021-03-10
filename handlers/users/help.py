f4f1a501995e4afe6347c7514ae33fdc60ebade7
waydk <waydk.main@gmail.com>
2021-03-10 05:00:49 +0300

    added a sticker to help, changed the requirements

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -2,6 +2,7 @@ from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandHelp
 
 from loader import dp
+from stickers.dn_stickers import kira
 
 
 @dp.message_handler(CommandHelp())
@@ -12,5 +13,6 @@ async def bot_help(message: types.Message):
             "/rules - rules for the use of the death note"
             "/write_down - write in the death note\n"
             "/death_list - show victim list ")
-    
+
+    await message.answer_sticker(kira)
     await message.answer("\n".join(text))
