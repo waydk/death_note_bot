8687a8ee9c040f9599107bd1c2df003d71ddbd7b
Kira Kormak <77948380+waydk@users.noreply.github.com>
2021-03-22 11:23:23 +0300

    fixed the bug 🐛

--- src/handlers/users/write.py
+++ src/handlers/users/write.py
@@ -9,20 +9,16 @@ from src.utils.db_api import db_helpers
 async def write_in_death_note(message: types.Message):
     """A simplified /write_down function that allows you to write
     the victim to the database"""
-    name_victim = None
-    reason = None
     victim = message.text
     victim = victim.split(" ")
     user_id, victim_id = message.from_user.id, message.message_id
     try:
         name_victim = victim[1] + ' ' + victim[2]
         reason = victim[3]
-    except IndexError:
-        await message.answer("Incorrect input")
-    try:
         await db_helpers.add_victim(id_user=user_id, id_victim=victim_id, name_victim=name_victim, reason=reason)
         text = _("📓 {} was added to the death note, ☠ his cause of death: {}  "
                  "/death_list").format(name_victim, reason)
         await message.answer(text)
-    except UnboundLocalError:
-        pass
+    except IndexError:
+        await message.answer("Incorrect input")
+
