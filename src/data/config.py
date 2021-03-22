a8d1b71d7039d3df4f1bf5180c77b732c8bec801
Kira Kormak <77948380+waydk@users.noreply.github.com>
2021-03-22 11:22:16 +0300

    small corrections

--- src/data/config.py
+++ src/data/config.py
@@ -6,16 +6,13 @@ env = Env()
 env.read_env()
 
 BOT_TOKEN = env.str("BOT_TOKEN")
-ADMINS = env.list("admins")
-IP = env.str("ip")
-DB_HOST = IP
+ADMINS = env.list("ADMINS")
+PG_HOST = env.str("PGHOST")
 PGUSER = env.str("PGUSER")
 PGPASSWORD = env.str("PGPASSWORD")
 DATABASE = env.str("DATABASE")
-
-POSTGRES_URI = f"postgresql://{PGUSER}:{PGPASSWORD}@{DB_HOST}/{DATABASE}"
+POSTGRES_URI = f"postgresql://{PGUSER}:{PGPASSWORD}@{PG_HOST}/{DATABASE}"
 
 I18N_DOMAIN = 'death_note_bot'
 BASE_DIR = Path(__file__).parent.parent.parent
 LOCALES_DIR = BASE_DIR / 'locales'
-
