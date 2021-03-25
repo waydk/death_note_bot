8a33b6434ee4692cca0efecbdfa4ac231be3954e
Kira Kormak <77948380+waydk@users.noreply.github.com>
2021-03-25 09:42:24 +0300

    fixed priority 🐛

--- src/handlers/users/__init__.py
+++ src/handlers/users/__init__.py
@@ -1,9 +1,9 @@
 from .help import dp
 from .start import dp
 from .rules import dp
-from .write_down import dp
+from .settings import dp
 from .death_note_list import dp
 from .write import dp
-from .settings import dp
+from .write_down import dp
 
 __all__ = ["dp"]
