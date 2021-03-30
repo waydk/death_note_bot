a34b0a678f735639dc53aa5eb39dee9a505452f0
waydk <waydk.main@gmail.com>
2021-03-30 09:53:32 +0300

    Edits and adding a closing button to the settings

--- src/handlers/users/settings.py
+++ src/handlers/users/settings.py
@@ -2,24 +2,28 @@ from aiogram import types
 from aiogram.dispatcher.filters import Command
 from aiogram.types import CallbackQuery
 
-from src.keyboards.inline.language_keyboard import languages_markup
-from src.keyboards.inline.settings_keyboard import settings_callback, settings_markup
 from loader import _
 from loader import dp
+from src.keyboards.inline.close_keyboard import close_markup
+from src.keyboards.inline.language_keyboard import languages_markup
+from src.keyboards.inline.settings_keyboard import settings_callback, settings_markup
 from src.utils.db_api import db_helpers
 
 
 @dp.message_handler(Command("settings"), state='*')
 async def open_settings(message: types.Message):
     await message.delete()
-    await message.answer(_("Settings"), reply_markup=settings_markup)
+    await message.answer(_("⚙ Welcome to settings, here you can"
+                           " change your language!\n"
+                           "🇷🇺 or 🇺🇸 Click the button below"),
+                         reply_markup=settings_markup)
 
 
 @dp.callback_query_handler(settings_callback.filter(title="change_lang"), state='*')
 async def change_language(call: CallbackQuery):
+    await call.message.delete()
     await call.answer(cache_time=1)
     await call.message.answer(_("Select a language "), reply_markup=languages_markup)
-    await call.message.delete()
 
 
 @dp.callback_query_handler(text_contains="lang", state='*')
@@ -27,4 +31,4 @@ async def change_language(call: CallbackQuery):
     await call.message.edit_reply_markup()
     lang = call.data[-2:]
     await db_helpers.set_language(lang)
-    await call.message.answer(_("Your language has been changed", locale=lang))
+    await call.message.answer(_("Your language has been changed", locale=lang), reply_markup=close_markup)
--- src/keyboards/inline/settings_keyboard.py
+++ src/keyboards/inline/settings_keyboard.py
@@ -2,9 +2,12 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.callback_data import CallbackData
 
 from loader import _
+from src.keyboards.inline.callback_datas import close_callback
 
 settings_callback = CallbackData("settings", "title")
 text = _("Settings")
-settings_markup = InlineKeyboardMarkup()
+settings_markup = InlineKeyboardMarkup(row_width=1)
 settings_button = InlineKeyboardButton(text=text, callback_data=settings_callback.new("change_lang"))
+close_button = InlineKeyboardButton(text="❌", callback_data=close_callback.new("close"))
 settings_markup.insert(settings_button)
+settings_markup.insert(close_button)
