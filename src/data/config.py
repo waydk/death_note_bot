966b441cdf0d29793447bcc6dd503a87e4b76c18
waydk <waydk.main@gmail.com>
2021-03-30 08:57:00 +0300

    Russian rules added

--- src/data/config.py
+++ src/data/config.py
@@ -11,6 +11,7 @@ PG_HOST = env.str("PGHOST")
 PGUSER = env.str("PGUSER")
 PGPASSWORD = env.str("PGPASSWORD")
 DATABASE = env.str("DATABASE")
+
 POSTGRES_URI = f"postgresql://{PGUSER}:{PGPASSWORD}@{PG_HOST}/{DATABASE}"
 
 I18N_DOMAIN = 'death_note_bot'
